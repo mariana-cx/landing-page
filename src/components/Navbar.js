6d3c888a94d874404983d93116ac719251d3554c
mariana-cx <133844552+mariana-cx@users.noreply.github.com>
2023-08-04 17:08:10 -0300

    already

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,14 @@ import React from "react";
 
 export const Navbar=()=>{
     return(<nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+        <div className="col-2"></div>
     <div className="container-fluid">
       <a className="navbar-brand" href="#">Start Bootstrap</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="col-8"></div>
-      <div className="collapse navbar-collapse" id="navbarScroll">
+      <div className="col-5"></div>
+      <div className="collapse navbar-collapse col-5" id="navbarScroll">
         <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">Home</a>
