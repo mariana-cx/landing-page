2baff4aa18a85e0c952aaef3cbef7dca4fee727d
mariana-cx <133844552+mariana-cx@users.noreply.github.com>
2023-08-04 17:03:54 -0300

    navbar already

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,28 @@
 import React from "react";
 
 export const Navbar=()=>{
-    return(<nav className="navbar navbar-expand-lg bg-body-tertiary">
+    return(<nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">Navbar scroll</a>
+      <a className="navbar-brand" href="#">Start Bootstrap</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
+      <div className="col-8"></div>
       <div className="collapse navbar-collapse" id="navbarScroll">
         <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Link
-            </a>
-            <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+            <a className="nav-link" href="#">About</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" aria-disabled="true">Link</a>
+            <a className="nav-link" href="#">Services</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contact</a>
           </li>
         </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>)
