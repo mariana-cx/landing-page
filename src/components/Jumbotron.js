b3d0986d82b4d688177c47e64578f77b8da4f8ef
mariana-cx <133844552+mariana-cx@users.noreply.github.com>
2023-08-05 09:15:07 -0300

    jumbotron

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -6,10 +6,10 @@ export const Jumbotron = () => {
         <div className="bg-secondary-subtle container col-8">
             <div className="row fs-1 ms-2 mb-1">A Warm Welcome!</div>
             <div className="row ms-2 mb-2 text-start align-self-center d-block">
-                This is a simple hero unit, a simple jumbotron-style component for calling
-                extra attention to featured content or information.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt adit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.
             </div>
-            <div className="button" type="button" class="d-flex btn btn-primary justify-content-start p-2 mb-5 ms-2">Call to action!
+            <div className="d-inline p-2 d-flex flex-row">
+                <button className="btn btn-primary p-2 mb-5 ms-1">Call to action!</button>
             </div>
             <div className="col-2"></div>
         </div>
