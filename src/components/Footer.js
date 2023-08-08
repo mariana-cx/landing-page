63a2d33faae335c15ad42e2f4fdc6242c8d5dde7
mariana-cx <133844552+mariana-cx@users.noreply.github.com>
2023-08-08 00:01:17 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer=()=>{
     return(<div className="footer">
-        <div className="text-center bg-dark border-bottom border-body container-fluid p-4 text-bg-dark fw-light fs-6">Copyright Your Website 2023</div>
+        <div className="text-center bg-dark border-bottom border-body container-fluid p-5 text-bg-dark fw-light fs-6">Copyright Your Website 2023</div>
 
     </div>
 
