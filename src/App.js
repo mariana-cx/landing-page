9c0610500ca1086f6812e22b77ca64a58367e012
mariana-cx <133844552+mariana-cx@users.noreply.github.com>
2023-08-14 15:00:20 -0300

    loop

--- src/App.js
+++ src/App.js
@@ -3,16 +3,59 @@ import { Cards } from './components/Cards';
 import { Jumbotron } from './components/Jumbotron';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
-export const App=()=> {
+import React from 'react';
 
- 
+export const App = () => {
+
+  const cards = [{
+    img: "https://www.edumilestones.com/career-library/images/programming.png",
+    title: "First",
+    description: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+  }, {
+    img: "https://www.edumilestones.com/career-library/images/programming.png",
+    title: "First",
+    description: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+  }, {
+    img: "https://www.edumilestones.com/career-library/images/programming.png",
+    title: "First",
+    description: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+  }, {
+    img: "https://www.edumilestones.com/career-library/images/programming.png",
+    title: "First",
+    description: "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+  }]
 
   return (
     <div className="App">
-    <Navbar />
-    <Jumbotron/>
-    <Cards/>
-    <Footer/>
+      <Navbar />
+      <Jumbotron />
+
+
+
+
+      <div className="card-group mt-3 mb-3">
+        <div className="col-2"></div>
+        {cards.map((e, i) => {
+          const startMargin = "ms-2"
+          if (i === 0) {
+            return (
+              <React.Fragment key={i}>
+                <Cards content={e} isFirst={true} />
+              </React.Fragment>
+            )
+          } else {
+            return (<React.Fragment key={i}>
+              <Cards content={e} />
+            </React.Fragment>)
+          }
+        })}
+        <div className="col-2"></div>
+      </div>
+
+
+
+
+      <Footer />
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,48 +1,47 @@
 import React from "react";
 
-export const Cards=()=>{
-    return (<div className="card-group mt-3 mb-3">
-    <div className="col-2"></div>
-    <div className="card border rounded-top">
-      <img src="https://www.edumilestones.com/career-library/images/programming.png" className="card-img-top border img-fluid rounded-top" style={{ width: '300px', height: '150px' }} alt="Card 1" />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div className="card-footer">
-        <button type="button" class="btn btn-primary btn-sm">Find out more!</button>
-      </div>
-    </div>
-    <div className="card ms-2 border rounded-top">
-      <img src="https://leverageedublog.s3.ap-south-1.amazonaws.com/blog/wp-content/uploads/2020/01/21182224/software-engineering.jpg" className="card-img-top img-fluid border rounded-top" style={{ width: '300px', height: '150px' }} alt="Card 2"/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div className="card-footer">
-        <button type="button" class="btn btn-primary btn-sm">Find out more!</button>
-      </div>
-    </div>
-    <div className="card ms-2 border rounded-top">
-      <img src="https://img.freepik.com/premium-vector/programmer-work-laptop-computer-website-code-program-concept_133260-5402.jpg" className="card-img-top img-fluid border rounded-top" style={{ width: '300px', height: '150px' }} alt="Card 3"/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div className="card-footer">
-      <button type="button" class="btn btn-primary btn-sm">Find out more!</button>
-      </div>
-    </div>
-    <div className="card ms-2 border rounded-top">
-      <img src="https://img.freepik.com/free-vector/hand-drawn-flat-design-devops-illustration_23-2149375793.jpg" className="card-img-top img-fluid border rounded-top" style={{ width: '300px', height: '150px' }} alt="Card 4"/>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div className="card-footer">
-      <button type="button" class="btn btn-primary btn-sm">Find out more!</button>
-      </div>
-      </div>
-    <div className="col-2"></div>
-  </div>)
+export const Cards = (props) => {
+
+    return (<>
+        <div className={`card border rounded-top ${props.isFirst ? "" : "ms-2"}`}>
+            <img src="https://www.edumilestones.com/career-library/images/programming.png" className="card-img-top border img-fluid rounded-top" style={{ width: '300px', height: '150px' }} alt="Card 1" />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div className="card-footer">
+                <button type="button" class="btn btn-primary btn-sm">Find out more!</button>
+            </div>
+        </div>
+        {/* <div className="card ms-2 border rounded-top">
+            <img src="https://leverageedublog.s3.ap-south-1.amazonaws.com/blog/wp-content/uploads/2020/01/21182224/software-engineering.jpg" className="card-img-top img-fluid border rounded-top" style={{ width: '300px', height: '150px' }} alt="Card 2" />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            <div className="card-footer">
+                <button type="button" class="btn btn-primary btn-sm">Find out more!</button>
+            </div>
+        </div>
+        <div className="card ms-2 border rounded-top">
+            <img src="https://img.freepik.com/premium-vector/programmer-work-laptop-computer-website-code-program-concept_133260-5402.jpg" className="card-img-top img-fluid border rounded-top" style={{ width: '300px', height: '150px' }} alt="Card 3" />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+            <div className="card-footer">
+                <button type="button" class="btn btn-primary btn-sm">Find out more!</button>
+            </div>
+        </div>
+        <div className="card ms-2 border rounded-top">
+            <img src="https://img.freepik.com/free-vector/hand-drawn-flat-design-devops-illustration_23-2149375793.jpg" className="card-img-top img-fluid border rounded-top" style={{ width: '300px', height: '150px' }} alt="Card 4" />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+            </div>
+            <div className="card-footer">
+                <button type="button" class="btn btn-primary btn-sm">Find out more!</button>
+            </div>
+        </div> */}
+    </>)
 }
